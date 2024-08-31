b73a79ee31481d3fe619601dc921e58b5d393b17
Oskar <hamerlao@gmail.com>
2024-08-31 23:06:23 +0200

    refactored the app component, now holds the navBar and footer, alsho the container was created

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,5 +1,22 @@
+import { Footer } from "./components/Footer";
+import { GetRestaurants } from "./components/GetRestaurants";
+import { NavigationBar } from "./components/NavigationBar";
+
+import "./App.css";
+import mapImage from "./assets/map.png";
+import placesImage from "./assets/places.png";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavigationBar />
+      <div className="content-container">
+        <img src={placesImage} alt="Places" />
+        <GetRestaurants />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
