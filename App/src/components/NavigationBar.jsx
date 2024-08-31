ff73bdcb9d2aeff9546ebd4271fea403927d321d
Oskar <hamerlao@gmail.com>
2024-08-31 23:08:03 +0200

    Created the navigationBar component and css file

---
+++ App/src/components/NavigationBar.jsx
@@ -0,0 +1,17 @@
+import "./NavigationBar.css";
+
+export const NavigationBar = () => {
+  return (
+    <>
+      <div className="nav-bar">
+        <p>GASTRO-BEASTS</p>
+        <div className="nav-info">
+          <p>HOME</p>
+          <p>PRIVACY</p>
+          <p>CONTACT</p>
+        </div>
+        <p>ACCOUNT</p>
+      </div>
+    </>
+  );
+};
