3521188a9e4a0e884dfda1c54bb5a7f31b144c05
Oskar <hamerlao@gmail.com>
2024-09-02 20:35:07 +0200

    Added the routes; now routing to the current component of navbar

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,20 +1,27 @@
 import { Footer } from "./components/Footer";
 import { GetRestaurants } from "./components/GetRestaurants";
 import { NavigationBar } from "./components/NavigationBar";
-
 import "./App.css";
-import mapImage from "./assets/map.png";
 import placesImage from "./assets/places.png";
+import { RestaurantTile } from "./components/RestaurantTile";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { GastroInfo } from "./components/GastroInfo";
 
 function App() {
   return (
     <>
-      <NavigationBar />
-      <div className="content-container">
-        <img src={placesImage} alt="Places" />
-        <GetRestaurants />
-      </div>
-      <Footer />
+      <Router>
+        <NavigationBar />
+        <div className="content-container">
+          <img src={placesImage} alt="Places" />
+          <Routes>
+            <Route path="gastroInfo" element={<GastroInfo />} />
+            <Route path="/" element={<RestaurantTile />} />
+            <Route path="/beasts" element={<GetRestaurants />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </>
   );
 }
