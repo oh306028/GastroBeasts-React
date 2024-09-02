3415047951ea16ff2331da900a5476409100c03b
Oskar <hamerlao@gmail.com>
2024-09-02 20:36:15 +0200

    refactored the navbar; now every link includes the old p tag

--- App/src/components/NavigationBar.jsx
+++ App/src/components/NavigationBar.jsx
@@ -1,17 +1,25 @@
 import "./NavigationBar.css";
+import { Link } from "react-router-dom";
 
 export const NavigationBar = () => {
   return (
-    <>
-      <div className="nav-bar">
+    <div className="nav-bar">
+      <Link to="gastroInfo">
         <p>GASTRO-BEASTS</p>
-        <div className="nav-info">
+      </Link>
+
+      <div className="nav-info">
+        <Link to="/">
           <p>HOME</p>
-          <p>PRIVACY</p>
+        </Link>
+        <Link to="/beasts">
+          <p>BEASTS</p>
+        </Link>
+        <Link to="/contact">
           <p>CONTACT</p>
-        </div>
-        <p>ACCOUNT</p>
+        </Link>
       </div>
-    </>
+      <p>ACCOUNT</p>
+    </div>
   );
 };
