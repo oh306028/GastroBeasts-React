8ab5b5f63e6fdf50b20fd64ed9cfbef672218a15
Oskar <hamerlao@gmail.com>
2024-09-03 00:25:47 +0200

    created the route for login and register

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -2,24 +2,26 @@ import { Footer } from "./components/Footer";
 import { GetRestaurants } from "./components/GetRestaurants";
 import { NavigationBar } from "./components/NavigationBar";
 import "./App.css";
-import placesImage from "./assets/places.png";
 import { RestaurantTile } from "./components/RestaurantTile";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GastroInfo } from "./components/GastroInfo";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
 
 function App() {
   return (
     <>
       <Router>
         <NavigationBar />
-        <div className="content-container">
-          <img src={placesImage} alt="Places" />
-          <Routes>
-            <Route path="gastroInfo" element={<GastroInfo />} />
-            <Route path="/" element={<RestaurantTile />} />
-            <Route path="/beasts" element={<GetRestaurants />} />
-          </Routes>
-        </div>
+
+        <Routes>
+          <Route path="gastroInfo" element={<GastroInfo />} />
+          <Route path="/" element={<RestaurantTile />} />
+          <Route path="/beasts" element={<GetRestaurants />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register></Register>} />
+        </Routes>
+
         <Footer />
       </Router>
     </>
