fb11b0030a3218f7b8170d5742ecc43e736ab4cc
Oskar <hamerlao@gmail.com>
2024-09-04 00:05:13 +0200

    cleaned the images for pages

--- App/src/components/RestaurantsList.jsx
+++ App/src/components/RestaurantsList.jsx
@@ -2,6 +2,22 @@ import { useEffect, useState } from "react";
 import "./RestaurantList.css";
 import { GetCategories } from "./RestaurantTile";
 
+export const RenderAverageStars = ({ reviews }) => {
+  if (!Array.isArray(reviews) || reviews.length === 0) {
+    return <p>Avg Stars: no reviews</p>;
+  }
+
+  const avgStars =
+    reviews.reduce((sum, review) => sum + review.stars.star, 0) /
+    reviews.length;
+
+  return (
+    <>
+      <p>Avg Stars: {avgStars.toFixed(1)}</p>
+    </>
+  );
+};
+
 export const RestaurantList = () => {
   const [restaurants, setRestaurants] = useState([]);
 
@@ -16,22 +32,6 @@ export const RestaurantList = () => {
     fetchData();
   }, []);
 
-  const RenderAverageStars = ({ reviews }) => {
-    if (!Array.isArray(reviews) || reviews.length === 0) {
-      return <p>Avg Stars: no reviews</p>;
-    }
-
-    const avgStars =
-      reviews.reduce((sum, review) => sum + review.stars.star, 0) /
-      reviews.length;
-
-    return (
-      <>
-        <p>Avg Stars: {avgStars.toFixed(1)}</p>
-      </>
-    );
-  };
-
   const RenderRestaurantTile = () => {
     return (
       <>
@@ -42,6 +42,10 @@ export const RestaurantList = () => {
               <ul className="list-categories-container">
                 <GetCategories categories={r.categories} />
               </ul>
+
+              <h3>{r.address.city}</h3>
+              <h3>{r.address.street}</h3>
+              <h3>{r.address.number}</h3>
               <p>{r.description}</p>
               <RenderAverageStars reviews={r.reviews} />
             </div>
@@ -51,12 +55,53 @@ export const RestaurantList = () => {
     );
   };
 
+  const GenerateAllCategories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+      const fetchCategories = async () => {
+        const response = await fetch(
+          "http://localhost:5194/api/restaurants/categories"
+        );
+        const result = await response.json();
+        setCategories(result);
+      };
+      fetchCategories();
+    }, []);
+
+    return (
+      <>
+        {categories.map((c) => (
+          <li key={c.id}>{c.name}</li>
+        ))}
+      </>
+    );
+  };
+
   return (
     <>
-      <div className="beast-list-container">
-        <ul>
-          <RenderRestaurantTile />
-        </ul>
+      <div className="beast-page-container">
+        <div className="categories-list-menu">
+          <h1>Categories</h1>
+          <ul>
+            <GenerateAllCategories />
+          </ul>
+          <hr />
+          <h1>Stars</h1>
+          <ul>
+            <li>5</li>
+            <li>4</li>
+            <li>3</li>
+            <li>2</li>
+            <li>1</li>
+          </ul>
+        </div>
+
+        <div className="beast-list-container">
+          <ul className="beast-tiles-list">
+            <RenderRestaurantTile />
+          </ul>
+        </div>
       </div>
     </>
   );
