6ebc3d56ab22670a574394a684caa8f915c0f967
Oskar <hamerlao@gmail.com>
2024-09-06 00:08:12 +0200

    refactored the beast details componnent; holds every information; added default stylish

--- App/src/components/BeastDetails.jsx
+++ App/src/components/BeastDetails.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { GetCategories } from "./RestaurantTile";
+import "./BeastDetails.css";
+import navigationImg from "../assets/navigation.png";
+import { RenderAverageStars } from "./RestaurantsList";
+import { ReviewList } from "./ReviewList";
 
 export const BeastDetails = () => {
   const location = useLocation();
@@ -7,7 +12,7 @@ export const BeastDetails = () => {
 
   const [restaurant, setRestaurant] = useState({});
 
-  console.log(data.reviews);
+  console.log(data);
 
   useEffect(() => {
     setRestaurant(data);
@@ -24,13 +29,37 @@ export const BeastDetails = () => {
   return (
     <>
       <p>ITS A DETAIL!</p>
-      <h1>{restaurant.name}</h1>
 
-      <ul>
-        {restaurant.reviews.map((r) => (
-          <li key={r.id}>{r.comment}</li>
-        ))}
-      </ul>
+      <div className="details-container">
+        <div className="details-info">
+          <h1 className="title">{restaurant.name}</h1>
+          <ul className="list-categories-container">
+            <GetCategories categories={restaurant.categories} />
+          </ul>
+          <ul className="address-container">
+            <li>
+              <img className="navigationImg" src={navigationImg} />
+            </li>
+            <li>
+              <h4>{restaurant.address.city},</h4>
+            </li>
+            <li>
+              <h4>{restaurant.address.street}</h4>
+            </li>
+            <li>
+              <h4>{restaurant.address.number}</h4>
+            </li>
+          </ul>
+          <p>{restaurant.description}</p>
+          <RenderAverageStars reviews={restaurant.reviews} />
+        </div>
+
+        <div className="vertical-line"></div>
+
+        <div className="reviews-container">
+          <ReviewList reviews={restaurant.reviews} />
+        </div>
+      </div>
     </>
   );
 };
