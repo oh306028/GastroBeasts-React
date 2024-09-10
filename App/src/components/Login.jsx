d32549f94abe2b544c04426ddc74952c0698bab4
Oskar <hamerlao@gmail.com>
2024-09-10 22:16:49 +0200

    refactored the login componnent, now it can handle the submit button, sending the post request and gets the JWT token

--- App/src/components/Login.jsx
+++ App/src/components/Login.jsx
@@ -1,19 +1,51 @@
 import "./Login.css";
 import { Link } from "react-router-dom";
 import foodImg from "../assets/food.png";
+import { getToken, loginUser, registerUser, saveToken } from "./Authentication";
+import { useState } from "react";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const loginData = {
+      email: email,
+      password: password,
+    };
+
+    const token = await loginUser(loginData);
+    saveToken(token);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <>
       <div className="login-page-container">
         <img src={foodImg} />
         <div className="login-container">
           <form className="login-form">
-            <input type="text" placeholder="Email"></input>
+            <input
+              onChange={handleEmail}
+              type="text"
+              placeholder="Email"
+            ></input>
 
-            <input type="password" placeholder="Password"></input>
+            <input
+              onChange={handlePassword}
+              type="password"
+              placeholder="Password"
+            ></input>
 
-            <button className="submit" type="submit">
+            <button onClick={handleSubmit} className="submit" type="submit">
               Login
             </button>
             <Link to="/register">Dont have account?</Link>
