e276073f0d71661744a91f35edb8d0930833b6e6
Oskar <hamerlao@gmail.com>
2024-09-11 16:43:01 +0200

    created the form and all labes and inputs for new beast

--- App/src/components/CreateBeast.jsx
+++ App/src/components/CreateBeast.jsx
@@ -1,8 +1,65 @@
+import { useState, useEffect } from "react";
+
 export const CreateBeast = () => {
+  const GenerateCategoriesCheckbox = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+      const fetchCategories = async () => {
+        const response = await fetch(
+          "http://localhost:5194/api/restaurants/categories"
+        );
+        const result = await response.json();
+        setCategories(result);
+      };
+      fetchCategories();
+    }, []);
+
+    return (
+      <>
+        {categories.map((c) => (
+          <ul>
+            <li key={c.id}>
+              <input id={c.name} type="checkbox"></input>
+              <label for={c.name}>{c.name}</label>
+            </li>
+          </ul>
+        ))}
+      </>
+    );
+  };
+
   return (
     <>
-      <h1>WOHO</h1>
-      <p>Creating new ....</p>
+      <h1>dd</h1>
+      <div className="form-container">
+        <form>
+          <label for="restName">Restaurant name</label>
+          <input
+            id="restName"
+            placeholder="restaurant name"
+            type="text"
+          ></input>
+          <label>Categories</label>
+          <GenerateCategoriesCheckbox />
+
+          <label for="City">City</label>
+          <input type="text" id="City" placeholder="City"></input>
+
+          <label for="Street">City</label>
+          <input type="text" id="Street" placeholder="Street"></input>
+
+          <label for="Number">City</label>
+          <input type="text" id="Number" placeholder="Number"></input>
+
+          <label for="descr">Description</label>
+          <textarea
+            id="descr"
+            type="text"
+            placeholder="Write the restaurant description"
+          ></textarea>
+        </form>
+      </div>
     </>
   );
 };
