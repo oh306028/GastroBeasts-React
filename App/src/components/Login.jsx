033302605f6acd1fd5de18aaa93cc72e20b1169a
Oskar <hamerlao@gmail.com>
2024-10-10 12:59:20 +0200

    Added error to the login page; added styles - now span messages informs about validation

--- App/src/components/Login.jsx
+++ App/src/components/Login.jsx
@@ -8,6 +8,32 @@ import { useNavigate } from "react-router-dom";
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, SetError] = useState({
+    email: "",
+    password: "",
+  });
+
+  const validateForm = (data) => {
+    const newError = {
+      email: "",
+      password: "",
+    };
+
+    let isValidData = true;
+
+    if (data.email.length === 0) {
+      newError.email = "Email cannot be empty!";
+      isValidData = false;
+    }
+
+    if (data.password.length === 0) {
+      newError.password = "Password cannot be empty!";
+      isValidData = false;
+    }
+
+    SetError(newError);
+    return isValidData;
+  };
 
   const navigate = useNavigate();
 
@@ -18,15 +44,21 @@ export const Login = () => {
       password: password,
     };
 
-    const refreshPage = () => {
-      window.location.reload();
-    };
+    const isValid = validateForm(loginData);
 
-    const token = await loginUser(loginData);
-    saveToken(token);
-    if (getToken() != null) {
-      navigate("/beasts");
-      refreshPage();
+    if (!isValid) {
+      return;
+    } else {
+      const refreshPage = () => {
+        window.location.reload();
+      };
+
+      const token = await loginUser(loginData);
+      saveToken(token);
+      if (getToken() != null) {
+        navigate("/beasts");
+        refreshPage();
+      }
     }
   };
 
@@ -49,12 +81,14 @@ export const Login = () => {
               type="text"
               placeholder="Email"
             ></input>
+            {error.email && <span>{error.email}</span>}
 
             <input
               onChange={handlePassword}
               type="password"
               placeholder="Password"
             ></input>
+            {error.password && <span>{error.password}</span>}
 
             <button onClick={handleSubmit} className="submit" type="submit">
               Login
